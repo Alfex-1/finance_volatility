009c60c4f042671cda0bf16993c64eda608f2037
Alfex-1 <alexandrebrunet@free.fr>
2024-12-26 21:51:25 +0100

    update

--- src/app/_main_.py
+++ src/app/_main_.py
@@ -760,21 +760,21 @@ elif option == "Prédiction" and len(selected_companies) >= 1 and end_date and d
             kurt_val, skewness_val = distribution(resid)
 
             if all(df_val['Respect']) == 1:
-                dist = 'normal'
+                dist='normal'
                 rolling_pred(real_values=df_pivot[col], train=train, test_size=test_size, vol="GARCH", p=p, q=q, mean=mean_t, dist=dist, col=col)
-                forecasting_volatility(data=train, model=model,vol='GARCH', p=p, q=q, mean=mean_t, dist='normal', col=col, horizon=horizon)
+                forecasting_volatility(data=df_pivot[col], model=model,vol='GARCH', p=p, q=q, mean=mean_t, dist='normal', col=col, horizon=horizon)
                 break
             else:
                 # Choisir le meilleur modèle selon la violation des hypothèses et la distribution des résidus
                 mean, dist = mean_dist(df_val, train, kurt_val, skewness_val)
-                
+        
                 # Recherche des meilleurs hyperparamètres
                 if mean == 'AR':
                     p, q, lag = ARCH_search(train, p_max=10, q_max=10, vol='GARCH', mean=mean, dist=dist, criterion='aic')
                     
                     # Construction du meilleur modèle selon le critère d'information
                     model = arch_model(train, vol='GARCH', p=p, q=q, mean=mean, dist=dist, lags=lag,  rescale=False)
-                
+
                 
                 else:
                     lag=None
@@ -782,34 +782,35 @@ elif option == "Prédiction" and len(selected_companies) >= 1 and end_date and d
                 
                     # Construction du meilleur modèle selon le critère d'information
                     model = arch_model(train, vol='GARCH', p=p, q=q, mean=mean, dist=dist, rescale=False)
-                
+        
                 model = model.fit(disp='off', options={'maxiter': 750})
-                
+        
                 # Validation
                 df_val = model_validation(model)
-
+                
                 # Prédictions glissantes
                 rolling_pred(real_values=df_pivot[col], test_size=test_size, vol='GARCH', p=p, q=q, mean=mean, dist=dist, col=col, lag=lag)
                 forecasting_volatility(data=df_pivot[col], model=model,vol='GARCH', p=p, q=q, mean=mean, dist=dist, col=col, lag=lag, horizon=horizon)
-
-            # Ajouter les informations du modèle à la liste
-            model_summary.append({
-            'Entreprise': col,
-            'Ordre p': p,
-            'Ordre q': q,
-            'Moyenne': mean,
-            "Distribution d'erreur": dist,
-            'Retard' : "None" if mean != 'AR' else lag})
-            
-            # Ajouter les informations sur le respect des hypothèses
-            model_val.append({
-                'Entreprise': col,
-                'Normalité des résidus': "Oui" if df_val.loc[df_val['Hypothèse'] == 'Normalité des résidus', 'Respect'].values[0] == 1 else "Non",
-                'Indépendance des résidus': "Oui" if df_val.loc[df_val['Hypothèse'] == 'Autocorrélation des résidus', 'Respect'].values[0] == 1 else "Non",
-                'Indépendance des résidus au carré': "Oui" if df_val.loc[df_val['Hypothèse'] == 'Autocorrélation des résidus au carré', 'Respect'].values[0] == 1 else "Non",
-                'Homoscédasticité conditionnelle': "Oui" if df_val.loc[df_val['Hypothèse'] == 'Effet ARCH', 'Respect'].values[0] == 1 else "Non",
-                'Stationnarité conditionnelle': "Oui" if df_val.loc[df_val['Hypothèse'] == 'Stationnarité conditionnelle', 'Respect'].values[0] == 1 else "Non"
-            })
+                        
+                # Ajouter les informations du modèle à la liste
+                model_summary.append({
+                    'Entreprise': col,
+                    'Ordre p': p,
+                    'Ordre q': q,
+                    'Moyenne': mean,
+                    "Distribution d'erreur": dist,
+                    'Retard' : "None" if mean != 'AR' else lag
+                })
+                    
+                # Ajouter les informations sur le respect des hypothèses
+                model_val.append({
+                    'Entreprise': col,
+                    'Normalité des résidus': "Oui" if df_val.loc[df_val['Hypothèse'] == 'Normalité des résidus', 'Respect'].values[0] == 1 else "Non",
+                    'Indépendance des résidus': "Oui" if df_val.loc[df_val['Hypothèse'] == 'Autocorrélation des résidus', 'Respect'].values[0] == 1 else "Non",
+                    'Indépendance des résidus au carré': "Oui" if df_val.loc[df_val['Hypothèse'] == 'Autocorrélation des résidus au carré', 'Respect'].values[0] == 1 else "Non",
+                    'Homoscédasticité conditionnelle': "Oui" if df_val.loc[df_val['Hypothèse'] == 'Effet ARCH', 'Respect'].values[0] == 1 else "Non",
+                    'Stationnarité conditionnelle': "Oui" if df_val.loc[df_val['Hypothèse'] == 'Stationnarité conditionnelle', 'Respect'].values[0] == 1 else "Non"
+                })
 
         # Informations des modèles
         model_summary_df = pd.DataFrame(model_summary)
