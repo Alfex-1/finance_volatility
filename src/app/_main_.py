6885db07995b2e09a44a759df6cc4bb2fdbc477d
Alfex-1 <alexandrebrunet@free.fr>
2024-12-28 12:16:59 +0100

    update

--- src/app/_main_.py
+++ src/app/_main_.py
@@ -373,6 +373,7 @@ def forecasting_volatility(data, model, vol, p, q, mean, dist, lag, col, horizon
     z_score = round(norm.ppf((1 + conf_level) / 2),3)
     conf_int_lower = np.sqrt(pred.variance.values[-1, :] - z_score * np.sqrt(pred.variance.values[-1, :])).round(3)
     conf_int_upper = np.sqrt(pred.variance.values[-1, :] + z_score * np.sqrt(pred.variance.values[-1, :])).round(3)
+    conf_int_lower = np.clip(conf_int_lower, 0, None)
 
     # Création du graphique interactif avec Plotly
     fig = go.Figure()
